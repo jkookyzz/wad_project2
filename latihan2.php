8d3c747e1910c26d30bbf252df163d7b71f009a7
jkookyzz <146175433+jkookyzz@users.noreply.github.com>
2023-10-21 11:53:46 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>latihan2_php</title>
+</head>
+<body>
+	<form>
+		<h1>Calculator</h1>
+		<label for="angka">Input angka:</label> 
+		<input type="number" id="angka" name="angka">
+
+		<label for="dropdown">Operation</label>
+		<select id="dropdown" name="Option">
+			<option value="Option1">+</option>
+			<option value="Option2">-</option>
+			<option value="Option3">*</option>
+		</select>
+		<button type="submit" class="btn btn-primary">Hitung</button>
+	</form>
+</html>
