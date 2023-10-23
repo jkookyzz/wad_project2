c97859b3ff53eec8a3a37246144b5a9e31253e6b
jkookyzz <146175433+jkookyzz@users.noreply.github.com>
2023-10-23 08:04:45 +0700

    Update latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -4,18 +4,44 @@
 	<title>latihan2_php</title>
 </head>
 <body>
-	<form>
-		<h1>Calculator</h1>
-		<label for="angka">Input angka:</label> 
-		<input type="number" id="angka" name="angka">
-
-		<label for="dropdown">Operation</label>
-		<select id="dropdown" name="Option">
-			<option value="Option1">+</option>
-			<option value="Option2">-</option>
-			<option value="Option3">*</option>
+	<h1>Calculator</h1>
+	<form method="POST" action="">
+		Input angka 1: <input type="text" name="angka1"><br>
+		Input angka 2: <input type="text" name="angka2"><br>
+		<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
 		</select>
-		<button type="submit" class="btn btn-primary">calculate</button>
+		<input type="submit" name="hitung" value="Hitung" class="tombol">
 	</form>
+	<?php
+	if (isset ($_POST['hitung'])){
+		$angka1 = $_POST['angka1'];
+		$angka2 = $_POST['angka2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $angka1+$angka2;
+			break;
+			case 'kurang':
+				$hasil = $angka1-$angka2;
+			break;
+			case 'kali':
+				$hasil = $angka1*$angka2;
+			break;
+			case 'bagi':
+				$hasil = $angka1/$angka2;
+			break;			
+		}
+
+	}
+	?>
+	<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>	
 </body>
 </html>
